5a0c758b181c89d4cd7a6a8e5302b7b173d53fce
victor-jvp <ing.vj15@gmail.com>
2022-09-20 21:46:02 -0400

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,27 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/setup', function() {
+    $credentials =[
+        'email' => 'admin@admin.com',
+        'password' => 'password'
+    ];
+
+    if(Auth::attempt($credentials)) {
+        $user = new \App\Models\User();
+
+        $user->name = "Admin";
+        $user->email = $credentials['email'];
+        $user->password = Hash::make($credentials['password']);
+
+        $user->save();
+
+        if (Auth::attempt($credentials)) {
+            $user = Auth::user();
+
+            $adminToken = $user->createToken('admin-token', ['create', 'update', 'delete']);
+        }
+    }
+});
